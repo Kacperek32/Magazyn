a452ba4ad5c287407a2ddf328b3214afae26a03c
Kacper Manterys <kacpermanterys@gmail.com>
2022-06-03 22:00:37 +0200

    Add logging and input

---
+++ magazyn.py
@@ -0,0 +1,8 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+command = input("Co chciałbyś zrobić? ")
+if command in ("exit"):
+    if command == "exit":
+     logging.info("Dzięki wielkie, do zobaczenia!")
+    exit()
